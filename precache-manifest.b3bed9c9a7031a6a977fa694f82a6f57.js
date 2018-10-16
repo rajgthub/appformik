79f188c86ed35d1758957e1331fb33a27b0dd949
raj <rraju12@gmail.com>
2018-10-16 22:42:43 +0100

    Updates

---
+++ precache-manifest.b3bed9c9a7031a6a977fa694f82a6f57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c08e665667239e9b3ba",
+    "url": "/appformik/static/js/runtime~main.0c08e665.js"
+  },
+  {
+    "revision": "18f84d68a98342661867",
+    "url": "/appformik/static/js/main.18f84d68.chunk.js"
+  },
+  {
+    "revision": "90ef64e836c0266f9223",
+    "url": "/appformik/static/js/1.90ef64e8.chunk.js"
+  },
+  {
+    "revision": "18f84d68a98342661867",
+    "url": "/appformik/static/css/main.c51d04fc.chunk.css"
+  },
+  {
+    "revision": "90ef64e836c0266f9223",
+    "url": "/appformik/static/css/1.f39c75df.chunk.css"
+  },
+  {
+    "revision": "af0a15bb30a650d227182d37edef100a",
+    "url": "/appformik/index.html"
+  }
+];
