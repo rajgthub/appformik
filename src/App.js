712bb18117de088cc8bcfd7889db4f45240971ae
raj <rraju12@gmail.com>
2018-10-16 15:12:27 +0100

    Add formik components:Form and Field

--- src/App.js
+++ src/App.js
@@ -2,45 +2,43 @@ import React from "react";
 import { withFormik, Form, Field } from "formik";
 import "./App.css";
 
-const App = ({ values, handleChange, handleSubmit }) => {
+const App = ({ values }) => {
   //keys of the return obj from line 14
-  console.log(handleChange);
-  
+  // console.log(handleChange);
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={values.email}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={values.password}
-        onChange={handleChange}
-      />
+    <Form>
+      {" "}
+      {/*onSubmit={handleSubmit no need! implicitly passed */}
+      <Field type="email" name="email" placeholder="Email" />
+      <Field type="password" name="password" placeholder="Password" />
+      <label htmlFor="update">
+        <Field type="checkbox" name="update" checked={values.update} />
+        Send updates
+      </label>
+      <Field component="select" name="plan">
+      <option value="free">Free</option>
+      <option value="paid">Paid</option>
+      </Field>
       <button type="submit">Submit</button>
-    </form>
+    </Form>
   );
 };
 const AppFormik = withFormik({
-  mapPropsToValues({ email, password }) {
+  mapPropsToValues({ email, password, update, plan }) {
     // <AppFormik email="rraju12@gmail.com" />
     return {
       email: email || "",
-      password: password || ''
+      password: password || "",
+      update: update || true,
+      plan: plan || ''
     };
   },
-  handleChange(e){
-    console.log('jhoih',e);
-
-  },
-  handleSubmit(values){
+  // handleChange(e) {
+  //   console.log("jhoih", e);
+  // },
+  handleSubmit(values) {
     console.log(values);
-    
   }
 })(App);
 export default AppFormik;
